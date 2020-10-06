ac997e418b6a3f584004c933b96f5584efad0581
mayur.kanojiya <kanojiyamayur@gmail.com>
2020-10-06 19:54:02 +0530

    feat: added eucledean distance

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import json
 from db_manager import DB
 import config
 import pickle
-from inference import infer
+from inference import infer, get_vector
 import re
 import string
 import nltk
+from scipy.spatial import distance
 
 current_dir = os.curdir
 # create the application object
@@ -19,15 +20,24 @@ app.config['DATABASE_PATH'] = os.path.join(os.curdir, 'db/db.sqlite')
 
 global kmeans
 global vectorizer
+global lda
 stopword = nltk.corpus.stopwords.words('english')
+
+
 def clean_text(text):
+    text = text.replace('[image: Google]', '')
     text_nopunct = "".join([char for char in text if char not in string.punctuation])
     tokens = re.split('\W+', text_nopunct)
+    tokens = [word.lower() for word in tokens]
     text = [word for word in tokens if word not in stopword]
+    text = [word for word in text if len(word) > 1]
+
     return text
+
 
 kmeans = pickle.load(open("kmeans.pkl", "rb"))
 vectorizer = pickle.load(open('tfidf.pickle', "rb"))
+lda = pickle.load(open('lda.pickle', "rb"))
 
 def init():
     global manager
@@ -67,19 +77,36 @@ API DEVELOPMENT
 '''
 
 
+def sort_list(list1, accordingto):
+    zipped_pairs = zip(accordingto, list1)
+    z = [x for _, x in sorted(zipped_pairs)]
+    return z
+
+
 @app.route('/get_relevant', methods=['POST'])
 def get_invite():
     result = {"error": 1}
     try:
 
         query = json.loads(request.data.decode('utf-8'))['query']
-        group = infer(query, kmeans, vectorizer)
+        group, query_vec = infer(query, kmeans, vectorizer, lda)
+
 
         resultDf = manager.get_emails(group[0])
 
         data = []
+        euclidean_score = []
         for index, row in resultDf.iterrows():
-            data.append(dict(row))
+            email_data = dict(row)
+
+            score = distance.euclidean(query_vec, get_vector(row['content'], vectorizer, lda))
+
+            if score < 0.5:
+                email_data['euclidean'] = score
+                data.append(email_data)
+                euclidean_score.append(score)
+
+        data = sorted(data, key=lambda k: k['euclidean'], reverse=False)
 
         result['data'] = data
         result["error"] = 0
@@ -88,6 +115,7 @@ def get_invite():
     except Exception as err:
         result["message"] = str(err)
         return make_response(json.dumps(result), 404)
+
 
 
 # start the server with the 'run()' method
